4ef49c13a0598eb67654bb44f9ed13552b139d35
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-19 20:12:09 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -997,7 +997,9 @@ class FreeplayState extends MusicBeatState
 				if (HelperFunctions.shouldBeHidden(songs[curSelected[freeplayType]].songName.toLowerCase()))
 					listenin = 'Freeplay - Listening to SOMETHING NEW :)';
 					
+				#if desktop
 				DiscordClient.changePresence(listenin, null);
+				#end
 	
 				music.stop();
 	
